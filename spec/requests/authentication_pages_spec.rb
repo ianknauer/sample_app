8c2032af4f27950f56220126398a93b417b6b4a0
Ian Knauer <ian.knauer@gmail.com>
2012-11-08 11:00:19 -0800

    End of 9.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,5 +38,16 @@ end
         it { should have_selector('Sign out', href: signin_path) }
         it { should_not have_link('Sign in', href: signin_path) } 
     end
-  end
+  
+  describe "with valid information" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user }
+    
+    it { should have_selector('title', text: user.name) }
+    it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Settings', href: edit_user_path(user)) }
+    it { should have_link('Sign out', href: signout_path) }
+    it { should_not have_link('Sign in', href: signin_path) }
+    end
+    end
 end
