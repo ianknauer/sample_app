7d9057d0b76bc406be93aaff4859da149a46b3af
Ian Knauer <ian.knauer@gmail.com>
2012-11-07 21:08:49 -0800

    Finished sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,9 +23,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation)}
-  
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
+  
     describe "when password is not present" do
        before { @user.password = @user.password_confirmation = " " }
        it { should_not be_valid }
@@ -59,5 +60,9 @@ describe User do
         it { should_not == user_for_invalid_password }
         specify { user_for_invalid_password.should be_false }
       end
+        describe "remember token" do
+          before { @user.save }
+          its(:remember_token) { should_not be_blank }
+        end
     end
   end
